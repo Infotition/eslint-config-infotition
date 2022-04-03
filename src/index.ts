5fa2d1258ce0710f7b905992e83b06fb63e5cf43
DevTobias <tobi.kaerst@gmx.de>
2022-04-03 22:01:14 +0200

    feat: added nodejs eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,55 @@
-import { hello } from "./react";
+import { Linter } from 'eslint';
 
-hello();
-console.log("World")
+const config: Linter.Config = {
+  settings: {
+    'import/resolver': { typescript: {} },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+    'import/no-default-export': 'error',
+    'import/order': ['error', { alphabetize: { order: 'asc' } }],
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+};
+
+export = config;
